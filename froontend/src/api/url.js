9dcb2bcb063046e00d0959660b49250cddaa68e0
Arun Yadav <ay20552291@gmail.com>
2025-12-15 23:47:25 +0530

    url

--- froontend/src/api/url.js
+++ froontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BACKEND_URL = axios.create({
-    baseURL: "http://localhost:5000/api/v1/noteapp/"
+    baseURL: "https://todo-app-1-qgga.onrender.com/"
 })
 
 export default BACKEND_URL
