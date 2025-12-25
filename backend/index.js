a72172dcaad9e5cd66c0c6f416a188a05890a2a4
Arun Yadav <ay20552291@gmail.com>
2025-12-25 16:45:33 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,11 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
